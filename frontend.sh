ed5799ebdb679e39c1aa805b971c66bdfa29c078
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-07-25 00:30:08 +0530

    frontend.script

---
+++ frontend.sh
@@ -0,0 +1,10 @@
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+yum install nginx -y
+rm -rf /etc/nginx/share/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html/
+unzip /tmp/frontend.zip
+systemctl enable nginx
+systemctl restart nginx
+
+
