5d35be70bf3a753d50874c9aceaeb57c59397c88
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-07-27 10:40:06 +0530

    added the log files

--- catalogue.sh
+++ catalogue.sh
@@ -1,46 +1,48 @@
-echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a catalogue service <<<<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+log=/tmp/catalogue.log
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a Mongodb repo <<<<<<<<<<<<<<<\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a catalogue service <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Downloading a Nodejs <<<<<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a Mongodb repo <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Installing a Nodejs <<<<<<<<<<<<<<<\e[0m"
-yum install nodejs -y
+echo -e "\e[33m>>>>>>>>>>>>>>>> Downloading a Nodejs <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a roboshop user <<<<<<<<<<<<<<<\e[0m"
-useradd roboshop
+echo -e "\e[33m>>>>>>>>>>>>>>>> Installing a Nodejs <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+yum install nodejs -y &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Deleting content in app folder <<<<<<<<<<<<<<<\e[0m"
-rm -rf /app
+echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a roboshop user <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+useradd roboshop &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a Application folder <<<<<<<<<<<<<<<\e[0m"
-mkdir /app
+echo -e "\e[33m>>>>>>>>>>>>>>>> Deleting content in app folder <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+rm -rf /app &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Downloading a Application code <<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[33m>>>>>>>>>>>>>>>> Creating a Application folder <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+mkdir /app &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Unzipping the Application code <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[33m>>>>>>>>>>>>>>>> Downloading a Application code <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+
+echo -e "\e[33m>>>>>>>>>>>>>>>> Unzipping the Application code <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Installing the Application Dependencies <<<<<<<<<<<<<<<\e[0m"
-npm install
+echo -e "\e[33m>>>>>>>>>>>>>>>> Installing the Application Dependencies <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+npm install &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Installing the Mongodb client <<<<<<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
+echo -e "\e[33m>>>>>>>>>>>>>>>> Installing the Mongodb client <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+yum install mongodb-org-shell -y &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Loading the mongodb schema <<<<<<<<<<<<<<<\e[0m"
-mongo --host mongodb.soujandevops.online </app/schema/catalogue.js
+echo -e "\e[33m>>>>>>>>>>>>>>>> Loading the mongodb schema <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+mongo --host mongodb.soujandevops.online </app/schema/catalogue.js &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Daemon service loading <<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
+echo -e "\e[33m>>>>>>>>>>>>>>>> Daemon service loading <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl daemon-reload &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Catalogue service enabled <<<<<<<<<<<<<<<\e[0m"
-systemctl enable catalogue
+echo -e "\e[33m>>>>>>>>>>>>>>>> Catalogue service enabled <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl enable catalogue &>>${log}
 
-echo -e "\e[33m>>>>>>>>>>>>>>>> Catalogue service restart <<<<<<<<<<<<<<<\e[0m"
-systemctl restart catalogue
+echo -e "\e[33m>>>>>>>>>>>>>>>> Catalogue service restart <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl restart catalogue &>>${log}
 
