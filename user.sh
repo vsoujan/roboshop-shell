f245ae1aa4e14145c1cb4f199be89eee971e72ae
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-07-30 18:38:09 +0530

    added the log files

--- user.sh
+++ user.sh
@@ -1,20 +1,4 @@
-cp user.service /etc/systemd/system/user.service
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+component=user
+source common.sh
 
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-npm install
-
-yum install mongodb-org-shell -y
-mongo --host mongodb.soujandevops.online </app/schema/user.js
-
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
-
-
+func_nodejs
